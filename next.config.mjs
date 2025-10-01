b830427187729099a4de6490b2b303da8332bb0c
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-10-01 00:19:19 -0300

    instalar y configurar Sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/sass")],
+    prependData: `@import "main.sass"`,
+  },
+};
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import { Header } from "./components/shared/Header";
-import { Footer } from "./components/shared/Footer";
+import { Header } from "../components/shared/Header";
+import { Footer } from "../components/shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
