acb59ff7fcd1013b8e9cd62515d8f272bd989dbe
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-10-02 11:36:22 -0300

    seo con nextjs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,12 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Future Wolrd",
+  description:
+    "Welcome to Future World an e-commerce platform from on the future",
+  keywords: ["ecomerce", "future", "world", "tecnology"],
+};
 
 export default function Home() {
   return (
