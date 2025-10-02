bcc2538628b4d8feecf2ab4447c292ebc220662d
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-10-02 20:09:05 -0300

    validacion del token de accesso

---
+++ src/utils/auth/validateAccessToken.ts
@@ -0,0 +1,13 @@
+import { cookies } from "next/headers";
+import { GraphQLClientSingleton } from "app/graphql";
+import { customerName } from "app/graphql/queries/customerName";
+
+export const validateAccessToken = async () => {
+  const cookiesStore = cookies();
+  const accessToken = cookiesStore.get("accessToken")?.value;
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+  const { customer } = await graphqlClient.request(customerName, {
+    customerAccessToken: accessToken,
+  });
+  return customer;
+};
